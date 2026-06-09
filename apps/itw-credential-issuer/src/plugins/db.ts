ad44034f9c5e46ce36b77a91cfcbb0548c3b3ae5
Benito-Cassa <benito.cassa@digitouch.it>
2026-06-09 16:17:17 +0200

    feat: integrate conformance hooks and session management in credentia… (#50)
    Co-authored-by: Benito Cassa <benito.cassa@external.mobilesoft.it>

--- apps/itw-credential-issuer/src/plugins/db.ts
+++ apps/itw-credential-issuer/src/plugins/db.ts
@@ -1,3 +1,4 @@
+import { SqliteConformanceSessionRepository } from '@itw-conformance-tool/conformance';
 import {
   DatabaseClient,
   SqliteNonceRepository,
@@ -6,10 +7,12 @@ import {
 } from '@itw-conformance-tool/database';
 import fp from 'fastify-plugin';
 
+import type { IConformanceSessionRepository } from '@itw-conformance-tool/conformance';
 import type { INonceRepository, IPARRepository, ISessionRepository } from '@itw-conformance-tool/database';
 
 declare module 'fastify' {
   interface FastifyInstance {
+    conformanceSessionRepository: IConformanceSessionRepository;
     dbClient: DatabaseClient;
     nonceRepository: INonceRepository;
     parRepository: IPARRepository;
@@ -24,6 +27,7 @@ export default fp(
       cleanupIntervalMs: app.config.DB_CLEANUP_INTERVAL_MS
     });
 
+    app.decorate('conformanceSessionRepository', new SqliteConformanceSessionRepository(dbClient.db));
     app.decorate('dbClient', dbClient);
     app.decorate('nonceRepository', new SqliteNonceRepository(dbClient.db));
     app.decorate('parRepository', new SqlitePARRepository(dbClient.db));
