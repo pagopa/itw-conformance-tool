051f5c4ec62376d43e897e0ae947f0e75d44cf57
Flavio Del Grosso <flavio.delgrosso@nttdata.com>
2026-07-24 14:04:59 +0200

    add toResult overloading

--- packages/utils/src/result.ts
+++ packages/utils/src/result.ts
@@ -1,10 +1,12 @@
 type Result<T, E extends Error> = { ok: true; value: T } | { ok: false; error: E };
 
-export const toResult = async <T, E extends Error>(value: T | PromiseLike<T>): Promise<Result<T, E>> => {
+export function toResult<T, E extends Error>(value: PromiseLike<T>): Promise<Result<T, E>>;
+export function toResult<T, E extends Error>(value: T): Promise<Result<T, E>>;
+export async function toResult<T, E extends Error>(value: T | PromiseLike<T>): Promise<Result<T, E>> {
   try {
     const result = await value;
     return { ok: true, value: result };
   } catch (error) {
     return { ok: false, error: error as E };
   }
-};
+}
